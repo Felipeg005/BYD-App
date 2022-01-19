6a90e55ea5699307c6e5ee85732027ce0869795e
Felipeg005 <juanfelipegomeztobon@gmail.com>
2022-01-19 17:40:46 -0500

    Add new view for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,34 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def new
+    @category = Category.new
+    @icons = ['icon-bill.png', 'icon-hospital.png', 'icon-hotel.png', 'icon-house.png', 'icon-mobile.png', 'icon-party.png',
+    'icon-restaurant.png', 'icon-taxi.png', 'icon-vacation.png', 'icon-money.png']
+  end
+
+  def create
+    if Recipe.new(recipes_params).invalid?
+      flash[:notice] = 'The Recipe was not saved for incorrect data'
+      redirect_to '/recipes/new/'
+    else
+      @recipe = Recipe.create!(recipes_params)
+      respond_to do |format|
+        format.html do
+          if @recipe.save
+            # success message
+            flash[:success] = 'Recipe saved successfully'
+            # redirect to index
+            redirect_to '/recipes/'
+          else
+            # error message
+            flash.now[:error] = 'Error: Recipe could not be saved'
+            # render new
+            render :new, locals: { recipe: @recipe }
+          end
+        end
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index', as: 'home'
   get 'categories', to: 'categories#index', as: 'categories'
+  get 'categories/new', to: 'categories#new', as: 'categories_new'
+
+  post 'categories/create', to: 'categories#create', as: 'create_new_category'
 end
