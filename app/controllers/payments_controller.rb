6be61fe2a3b827a9e374a8e0c860b1dd55b3ab7f
Felipeg005 <juanfelipegomeztobon@gmail.com>
2022-01-20 16:29:35 -0500

    Add new view and method for payments

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,4 +3,48 @@ class PaymentsController < ApplicationController
     @category = Category.find(params[:category_id])
     @payments = Payment.all
   end
+
+  def new
+    @payment = Payment.new
+  end
+
+  def create
+    if Payment.new(payment_params).invalid?
+      flash[:notice] = 'The payment was not saved for incorrect data'
+      redirect_to '/categories/new/'
+    else
+      @payment = Payment.create!(payment_params)
+      respond_to do |format|
+        format.html do
+          if @payment.save
+            # success message
+            flash[:success] = 'Payment saved successfully'
+            # redirect to index
+            redirect_to "/payments/#{params[:category_id]}"
+          else
+            # error message
+            flash.now[:error] = 'Error: Payment could not be saved'
+            # render new
+            render :new, locals: { payment: @payment }
+          end
+        end
+      end
+    end
+  end
+
+  def delete
+    @payment = Payment.find(params[:id])
+    if @payment.destroy
+      flash[:success] = 'payment destroyed successfully'
+    else
+      flash[:error] = 'Error: payment could not be destroyed'
+    end
+    redirect_back(fallback_location: payments_path)
+  end
+
+  private
+
+  def payment_params
+    params.require(:payment).permit(:name, :author_id, :icon)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index', as: 'home'
   get 'categories', to: 'categories#index', as: 'categories'
-  get 'payments/:category_id', to: 'payments#index', as: 'payments'
   get 'categories/new', to: 'categories#new', as: 'categories_new'
+  get 'payments/:category_id', to: 'payments#index', as: 'payments'
+  get 'payments/:category_id/new', to: 'payments#new', as: 'payments_new'
 
   post 'categories/create', to: 'categories#create', as: 'create_new_category'
   post 'categories/delete', to: 'categories#delete', as: 'delete_category'
