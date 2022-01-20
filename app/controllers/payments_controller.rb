8f8b64986466508969f51df4bd8b88757d9accfb
Felipeg005 <juanfelipegomeztobon@gmail.com>
2022-01-20 16:10:58 -0500

    Add route and index view for payments

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,6 @@
 class PaymentsController < ApplicationController
+  def index
+    @category = Category.find(params[:category_id])
+    @payments = Payment.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index', as: 'home'
   get 'categories', to: 'categories#index', as: 'categories'
-  get 'payments/:category_id', to: 'payments#show', as: 'show_payments'
+  get 'payments/:category_id', to: 'payments#index', as: 'payments'
   get 'categories/new', to: 'categories#new', as: 'categories_new'
 
   post 'categories/create', to: 'categories#create', as: 'create_new_category'
